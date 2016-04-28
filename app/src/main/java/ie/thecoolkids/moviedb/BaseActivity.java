9d8fb7e775c4a66799e63b44084f8b14fc1984ab
kfcobrien <kfcobrien@gmail.com>
2016-04-28 22:06:47 +0100

    Press enter to search

--- app/src/main/java/ie/thecoolkids/moviedb/BaseActivity.java
+++ app/src/main/java/ie/thecoolkids/moviedb/BaseActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
@@ -50,6 +51,23 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
         btnOpenNav = (ImageButton)findViewById(R.id.btnOpenNav);
         etQuery = (EditText)findViewById(R.id.etQuery);
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        etQuery.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER){
+                    if(!etQuery.getText().toString().equals("")){
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.putExtra("searchTerm", etQuery.getText().toString());
+                        startActivity(intent);
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Enter a query term", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                return false;
+            }
+        });
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
